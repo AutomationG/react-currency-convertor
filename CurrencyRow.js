b5d9610932c812230d40056b7cdcd5aa81853c57
AutomationG <pareek23gargi@gmail.com>
2020-06-23 02:43:25 +0530

    3 files modified, 1 added, 1 deleted

---
+++ CurrencyRow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function CurrencyRow(props){
+
+    return (
+      <>
+      <input type='number' value={props.amount} onChange={props.handleAmount}/>
+      <select value={props.baseCurrency} onChange={props.onChange}>
+      {props.options.map(op => (<option key={op} value={op}>{op}</option>))}
+      </select>
+      </>
+    );
+  }
--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import React, {useEffect,useState} from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
+import CurrencyRow from './CurrencyRow';
+import axios from 'axios';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+function App(){
+  const CURRENCY_URL = 'https://api.exchangeratesapi.io/latest';
+   const [currencyOption,setCurrencyOption] = useState([]);
+   const [fromCurrency,setFromCurrency] = useState('');
+   const [toCurrency,setToCurrency] = useState('');
+   const [amount,setAmount] = useState(1);
+   const [fromInput,setfromInput] = useState(true);
+   const [convCurrency,setConvCurrency] = useState()
 
-  render() {
+   let fromAmount,toAmount;
+   if(fromInput){
+     fromAmount = amount;
+     toAmount = amount * convCurrency
+   }else{
+     toAmount = amount;
+     fromAmount = amount / convCurrency
+   }
+   
+   useEffect(() => {
+     axios.get(CURRENCY_URL).then((response)=>{
+     const firstCurrency = Object.keys(response.data.rates)[0]
+     setCurrencyOption(Object.keys(response.data.rates))
+     setFromCurrency(response.data.base)
+     setToCurrency(firstCurrency)
+     setConvCurrency(response.data.rates[firstCurrency])
+     })
+   },[])
+
+    const handleFromAmount = (event) => {
+    setAmount(event.target.value)
+    setfromInput(true)
+    }
+
+    const handleToAmount = (event) => {
+    setAmount(event.target.value)
+    setfromInput(true)
+    }
+
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <>
+      <h1>Convertor</h1>
+      <CurrencyRow options={currencyOption} amount={amount} baseCurrency={fromCurrency} 
+       handleAmount = {handleFromAmount}
+      onChange={ event => setFromCurrency(event.target.value)}/>
+      <div className='equals'>=</div>
+      <CurrencyRow options={currencyOption} amount={convCurrency} baseCurrency={toCurrency} 
+      handleAmount = {handleToAmount}
+       onChange={ event => setToCurrency(event.target.value)}/>
+      </>
     );
   }
-}
 
 render(<App />, document.getElementById('root'));
